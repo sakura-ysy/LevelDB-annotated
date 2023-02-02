22b599503be76032fed0400e3ea2ff96f30ef020
YSY <2575633193@qq.com>
2023-02-02 16:53:18 +0800

    Version相关

---
+++ db/version_edit.h
@@ -0,0 +1,120 @@
+// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+
+#ifndef STORAGE_LEVELDB_DB_VERSION_EDIT_H_
+#define STORAGE_LEVELDB_DB_VERSION_EDIT_H_
+
+#include <set>
+#include <utility>
+#include <vector>
+
+#include "db/dbformat.h"
+
+namespace leveldb {
+
+class VersionSet;
+
+// SST文件元数据，用来记录一些信息
+struct FileMetaData {
+  FileMetaData() : refs(0), allowed_seeks(1 << 30), file_size(0) {}
+
+  int refs;
+  // 详见version_set.cc第650行左右的注释
+  int allowed_seeks;  // Seeks allowed until compaction
+  // 每个SST都有唯一的number（标识符？）
+  uint64_t number;  
+  uint64_t file_size;    // File size in bytes
+  InternalKey smallest;  // Smallest internal key served by table
+  InternalKey largest;   // Largest internal key served by table
+};
+
+// Version1 + VersionEdit = Version2
+// VersionEdit用来抽象一种动作，记录新增的SST（new_files_）和待删除的SST（deleted_files_）
+// 一次Compact就会生成一个VersionEdit
+class VersionEdit {
+ public:
+  VersionEdit() { Clear(); }
+  ~VersionEdit() = default;
+
+  void Clear();
+
+  void SetComparatorName(const Slice& name) {
+    has_comparator_ = true;
+    comparator_ = name.ToString();
+  }
+  void SetLogNumber(uint64_t num) {
+    has_log_number_ = true;
+    log_number_ = num;
+  }
+  void SetPrevLogNumber(uint64_t num) {
+    has_prev_log_number_ = true;
+    prev_log_number_ = num;
+  }
+  void SetNextFile(uint64_t num) {
+    has_next_file_number_ = true;
+    next_file_number_ = num;
+  }
+  void SetLastSequence(SequenceNumber seq) {
+    has_last_sequence_ = true;
+    last_sequence_ = seq;
+  }
+  void SetCompactPointer(int level, const InternalKey& key) {
+    compact_pointers_.push_back(std::make_pair(level, key));
+  }
+
+  // Add the specified file at the specified number.
+  // REQUIRES: This version has not been saved (see VersionSet::SaveTo)
+  // REQUIRES: "smallest" and "largest" are smallest and largest keys in file
+  void AddFile(int level, uint64_t file, uint64_t file_size,
+               const InternalKey& smallest, const InternalKey& largest) {
+    FileMetaData f;
+    f.number = file;
+    f.file_size = file_size;
+    f.smallest = smallest;
+    f.largest = largest;
+    new_files_.push_back(std::make_pair(level, f));
+  }
+
+  // Delete the specified "file" from the specified "level".
+  void RemoveFile(int level, uint64_t file) {
+    deleted_files_.insert(std::make_pair(level, file));
+  }
+
+  void EncodeTo(std::string* dst) const;
+  Status DecodeFrom(const Slice& src);
+
+  std::string DebugString() const;
+
+ private:
+  friend class VersionSet;
+
+  // int是SST所在Level号，uint64_t是文件编号number
+  typedef std::set<std::pair<int, uint64_t>> DeletedFileSet;
+
+  std::string comparator_;
+  uint64_t log_number_;
+  uint64_t prev_log_number_;
+  uint64_t next_file_number_;
+  SequenceNumber last_sequence_;
+  bool has_comparator_;
+  bool has_log_number_;
+  bool has_prev_log_number_;
+  bool has_next_file_number_;
+  bool has_last_sequence_;
+
+  // Version在Compact之后，会计算出每一个level上下一个要Compact的key，
+  // 注意，是下一次，而不是本次。
+  // 但是这个信息记录在VersionEdit上，而不是Version上，
+  // 该信息会在Apply()中通过builder.vset_保存到VersionSet中。
+  std::vector<std::pair<int, InternalKey>> compact_pointers_;
+  // 记录要删除的SST
+  DeletedFileSet deleted_files_;
+  // 记录要新建的SST
+  // int是SST所在Level号，FileMetaData就是SST元数据
+  std::vector<std::pair<int, FileMetaData>> new_files_;
+};
+
+}  // namespace leveldb
+
+#endif  // STORAGE_LEVELDB_DB_VERSION_EDIT_H_
