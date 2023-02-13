b59a03d0a8ce9a73323ae32b63fc9c58c389374b
YSY <2575633193@qq.com>
2023-02-13 11:51:01 +0800

    Arena内存分配器

--- util/arena.cc
+++ util/arena.cc
@@ -17,7 +17,18 @@ Arena::~Arena() {
   }
 }
 
+
+// 对AllocateNewBlock进行一次封装
+// Arena中以block为单位分配内存，一个block最大为kBlockSize
+// 如果bytes>1/4 kBlockSize，那么直接为其分配一个bytes大小的block
+// 如果bytes<=1/4 kBlockSize，那么申请一个kBlockSize大小的block
+// 并从中取出bytes的内存空间给它使用，预留下的空间可以供下一次分配
 char* Arena::AllocateFallback(size_t bytes) {
+
+  // 当bytes>1/4 kBlockSize时，如果为其申请一个完整的block（kBlockSize）
+  // 那么余下的空间最多只有3/4 kBlockSize，Arena认为其为容易浪费的碎片
+  // 一次直接分配一个bytes大小的block给它，不留剩余
+  // 当然，如果bytes很大，那么分配的空间就不止一个block了
   if (bytes > kBlockSize / 4) {
     // Object is more than a quarter of our block size.  Allocate it separately
     // to avoid wasting too much space in leftover bytes.
@@ -26,21 +37,39 @@ char* Arena::AllocateFallback(size_t bytes) {
   }
 
   // We waste the remaining space in the current block.
+  // bytes<=1/4 kBlockSize，直接申请一个kBlockSize大小的block
   alloc_ptr_ = AllocateNewBlock(kBlockSize);
   alloc_bytes_remaining_ = kBlockSize;
 
+  // 从中扣出bytes大小的空间给它
   char* result = alloc_ptr_;
   alloc_ptr_ += bytes;
   alloc_bytes_remaining_ -= bytes;
   return result;
 }
-
+ 
+// 对齐分配
 char* Arena::AllocateAligned(size_t bytes) {
+  // sizeof(void*)的值与目标平台位数有关
+  // 32位：sizeof(void*) == 4
+  // 64位：sizeof(void*) == 8
+  // 默认按8 bytes对齐
+  // 即要求地址为0、8、16、24、32...
   const int align = (sizeof(void*) > 8) ? sizeof(void*) : 8;
   static_assert((align & (align - 1)) == 0,
                 "Pointer size should be a power of 2");
+  
+  // 取得当前地址未对齐的尾数，即超出的部分
+  // 比如，要对齐的要求是8bytes
+  // 但是当前指针指向的是0x017（23）这里。
+  // 那么current_mod就是7
   size_t current_mod = reinterpret_cast<uintptr_t>(alloc_ptr_) & (align - 1);
+  // 取出要补齐的部分
+  // 如果当前地址是0x17, 要求对齐是8 bytes
+  // 那么current_mod = 7, slop就是1
   size_t slop = (current_mod == 0 ? 0 : align - current_mod);
+
+  // 要补齐的部分算在申请者头上
   size_t needed = bytes + slop;
   char* result;
   if (needed <= alloc_bytes_remaining_) {
@@ -55,6 +84,11 @@ char* Arena::AllocateAligned(size_t bytes) {
   return result;
 }
 
+
+// new一个新块
+// 把块指针push进block_中
+// 增加memory_usage_
+// 返回块指针
 char* Arena::AllocateNewBlock(size_t block_bytes) {
   char* result = new char[block_bytes];
   blocks_.push_back(result);
--- util/arena.h
+++ util/arena.h
@@ -13,6 +13,17 @@
 
 namespace leveldb {
 
+/**
+ * 为了防止出现内存碎片，LevelDB自己写了一套内存分配管理器，名称为Arena。
+ * 但要注意，Arena并不是为了整个LevelDB项目考虑的，主要是为了skiplist也就是memtable服务的
+ * memtable里记录了用户传进来的k-v，这些字符串有长有短，放到内存中时很容易导致内存碎片，
+ * 所以这里写了个统一的内存管理器。
+ * 
+ * memtable要申请内存时，就利用Arena分配器来分配内存。当memtable要释放时，就直接通过Arena中的
+ * block_把所有申请的内存释放掉
+*/
+
+
 class Arena {
  public:
   Arena();
@@ -23,13 +34,23 @@ class Arena {
   ~Arena();
 
   // Return a pointer to a newly allocated memory block of "bytes" bytes.
+  // 使用方法同malloc()
+  // 分配一片特定大小的内存，返回其首址
   char* Allocate(size_t bytes);
 
   // Allocate memory with the normal alignment guarantees provided by malloc.
+  // 分配对齐的内容
   char* AllocateAligned(size_t bytes);
+
+  // 为什么只有分配，没有释放？
+  // 1.因为memtable本身是没有删除k-v的操作的，只会一直的添加
+  // 2.memtable在flush之后，统一销毁掉，所以内存块可以直接由Arena来统一销毁
 
   // Returns an estimate of the total memory usage of data allocated
   // by the arena.
+  // 统计所有由Arena申请的内存总数
+  // 这里面可能包含一些内存碎片
+  // 所以返回的是近似值
   size_t MemoryUsage() const {
     return memory_usage_.load(std::memory_order_relaxed);
   }
@@ -39,30 +60,38 @@ class Arena {
   char* AllocateNewBlock(size_t block_bytes);
 
   // Allocation state
+  // 当前申请的内存块的指针
   char* alloc_ptr_;
+  // 已经申请的内存块，剩余的bytes数
   size_t alloc_bytes_remaining_;
 
   // Array of new[] allocated memory blocks
+  // 记录所有已经分配的内存块
   std::vector<char*> blocks_;
 
   // Total memory usage of the arena.
   //
   // TODO(costan): This member is accessed via atomics, but the others are
   //               accessed without any locking. Is this OK?
+  // Arena总共申请出去的内存条目
   std::atomic<size_t> memory_usage_;
 };
+
 
 inline char* Arena::Allocate(size_t bytes) {
   // The semantics of what to return are a bit messy if we allow
   // 0-byte allocations, so we disallow them here (we don't need
   // them for our internal use).
   assert(bytes > 0);
+  // 如果当前block余下的空间还够用
+  // 那就从中扣出bytes大小分配出去即可
   if (bytes <= alloc_bytes_remaining_) {
     char* result = alloc_ptr_;
     alloc_ptr_ += bytes;
     alloc_bytes_remaining_ -= bytes;
     return result;
   }
+  // 不够用了，就申请新block
   return AllocateFallback(bytes);
 }
 
